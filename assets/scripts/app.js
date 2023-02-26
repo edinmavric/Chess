614141610811622dfb1155a5d7356adff50aa2ee
edinmavric <edinmavric10@gmail.com>
2023-02-26 15:18:20 +0100

    initial commit

---
+++ assets/scripts/app.js
@@ -0,0 +1,7 @@
+const pawns = document.querySelectorAll(".fa-chess-pawn");
+const kings = document.querySelectorAll(".fa-chess-king");
+const queens = document.querySelectorAll(".fa-chess-queen");
+const rooks = document.querySelectorAll(".fa-chess-rook");
+const knights = document.querySelectorAll(".fa-chess-knight");
+const bishops = document.querySelectorAll(".fa-chess-bishop");
+
